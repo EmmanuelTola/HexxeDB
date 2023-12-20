4c8e4b044b142cc7c28e326eeafdf92ab0276bcb
Emmanuel Tola <106537091+EmmanuelTola@users.noreply.github.com>
2023-12-20 21:02:52 +0100

    Add files via upload

---
+++ xql.js
@@ -0,0 +1,16 @@
+let que = document.createElement("script");
+que.src = "xql/que.js"; 
+document.head.appendChild(que); 
+let query = document.createElement("script");
+query.src = "xql/query.js"; 
+document.head.appendChild(query); 
+let hexe = document.createElement("script");
+hexe.src = "xql/hex.js"; 
+document.head.appendChild(hexe); 
+let xcon = document.createElement("script");
+xcon.src = "xql/xcon.js"; 
+document.head.appendChild(xcon); 
+let css = document.createElement("link");
+css.href = "xql/hex.css"; 
+css.rel = "stylesheet"; 
+document.head.appendChild(css); 
